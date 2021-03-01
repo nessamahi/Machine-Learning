0c103c912859b94edde24287189f4132e9adc838
Iffatun Nessa <60021758+nessamahi@users.noreply.github.com>
2021-03-01 13:00:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,148 @@
-# Singular Value Decomposition
-# Given image was alreaady in greyscale
-from __future__ import division
-import matplotlib.pyplot as plt
+from scipy.spatial.distance import hamming
+from matplotlib import pyplot as p
+from sklearn.manifold import MDS
 import numpy as np
-from PIL import Image
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-image = np.array(Image.open(r"image.jpg"))
-# normalize the intensity values in each pixel
-image = image/255
+#reading fasta file as list
 
-row, col, _ = image.shape
-print("Pixels: ", row, " * ", col)
+instances = list(SeqIO.parse('HW2.fas', 'fasta'))
 
-# figure1 = plt.figure(figsize=(10, 5))
-# x = figure1.add_subplot(1, 1, 1)
-# imageplot = plt.imshow(image)
-# x.set_title('Cat')
-# plt.show()
-# image remains same as it is already in greyscale
-# next step is to sepearte greyscale values for each color. So, we will be working with 3 matrices with 2 dimension, and perform SVD decomposition for each color seperately.
-# using k = 100
+#Calculating hamming distance with python package
 
-imageRED = image[:, :, 0]
-imageGREEN = image[:, :, 1]
-imageBLUE = image[:, :, 2]
+dist = np.zeros((120, 120))
+for row in range(120):
+    for column in range(120):
+        dist[row, column] = hamming(instances[row], instances[column])
+        #print (dist)
 
-# originalBYTES = image.nbytes
-# print("To store the original image we need space in bytes is: ", originalBYTES)
 
-# performing SVD decomposition, Omega is written as D
-U_Red, D_Red, V_Red = np.linalg.svd(imageRED, full_matrices=True)
-U_Green, D_Green, V_Green = np.linalg.svd(imageGREEN, full_matrices=True)
-U_Blue, D_Blue, V_Blue = np.linalg.svd(imageBLUE, full_matrices=True)
+#Applying multidimentional scaling to plot data in 2D
 
-# bytes_to_be_stored = sum([matrix.nbytes for matrix in [U_Red, D_Red, V_Red, U_Green, D_Green, V_Green, U_Blue, D_Blue, V_Blue]])
-# print("The matrices have total size in bytes : ", bytes_to_be_stored)
+multi = MDS(dissimilarity='precomputed')
+data = multi.fit_transform(dist)
+	#print (data)
 
-k = 100
+# zip() - map the similar index of multiple containers
+p.scatter(*zip(*data))
+p.show()
 
-U_Red_K = U_Red[:, 0:k]
-D_Red_K = D_Red[0:k]
-V_Red_K = V_Red[0:k, :]
-U_Green_K = U_Green[:, 0:k]
-D_Green_K = D_Green[0:k]
-V_Green_K = V_Green[0:k, :]
-U_Blue_K = U_Blue[:, 0:k]
-D_Blue_K = D_Blue[0:k]
-V_Blue_k = V_Blue[0:k, :]
 
-# compressedBYTES = sum([matrix.nbytes for matrix in [U_Red_K, D_Red_K, V_Red_K, U_Green_K, D_Green_K, V_Green_K, U_Blue_K, D_Blue_K, V_Blue_k]])
-# print("Compressed bytes with k value 100 is : ", compressedBYTES)
 
-# ratio = compressedBYTES/originalBYTES
-# print("Ratio of the bytes : ", ratio)
+# K means cluster
+from numpy.linalg import norm
 
-# approximate matrices for each color and merging them. if the intensity of pixel is out of [0,1] this range, we need to fix it
+class Kmeans:
+    '''Implementing Kmeans algorithm.'''
 
-image_red_approx = np.dot(U_Red_K, np.dot(np.diag(D_Red_K), V_Red_K))
-image_green_approx = np.dot(U_Green_K, np.dot(np.diag(D_Green_K), V_Green_K))
-image_blue_approx = np.dot(U_Blue_K, np.dot(np.diag(D_Blue_K), V_Blue_k))
+    def __init__(self, n_clusters, max_iter=100, random_state=123):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.random_state = random_state
 
-image_reconstruct = np.zeros((row, col, 3))
+    def initializ_centroids(self, X):
+        np.random.RandomState(self.random_state)
+        random_idx = np.random.permutation(X.shape[0])
+        centroids = X[random_idx[:self.n_clusters]]
+        return centroids
 
-image_reconstruct[:, :, 0] = image_red_approx
-image_reconstruct[:, :, 1] = image_green_approx
-image_reconstruct[:, :, 2] = image_blue_approx
+    def compute_centroids(self, X, labels):
+        centroids = np.zeros((self.n_clusters, X.shape[1]))
+        for k in range(self.n_clusters):
+            centroids[k, :] = np.mean(X[labels == k, :], axis=0)
+        return centroids
 
-image_reconstruct[image_reconstruct < 0] = 0
-image_reconstruct[image_reconstruct > 1] = 1
+    def compute_distance(self, X, centroids):
+        distance = np.zeros((X.shape[0], self.n_clusters))
+        for k in range(self.n_clusters):
+            row_norm = norm(X - centroids[k, :], axis=1)
+            distance[:, k] = np.square(row_norm)
+        return distance
 
-figure1 = plt.figure(figsize=(10, 5))
-y = figure1.add_subplot(1, 1, 1)
-imageplot = plt.imshow(image_reconstruct)
+    def find_closest_cluster(self, distance):
+        return np.argmin(distance, axis=1)
 
-y.set_title('Cat with best rank {} approximation'.format(k))
+    def compute_sse(self, X, labels, centroids):
+        distance = np.zeros(X.shape[0])
+        for k in range(self.n_clusters):
+            distance[labels == k] = norm(X[labels == k] - centroids[k], axis=1)
+        return np.sum(np.square(distance))
+
+    def fit(self, X):
+        self.centroids = self.initializ_centroids(X)
+        for i in range(self.max_iter):
+            old_centroids = self.centroids
+            distance = self.compute_distance(X, old_centroids)
+            self.labels = self.find_closest_cluster(distance)
+            self.centroids = self.compute_centroids(X, self.labels)
+            if np.all(old_centroids == self.centroids):
+                break
+        self.error = self.compute_sse(X, self.labels, self.centroids)
+
+    def predict(self, X):
+        distance = self.compute_distance(X, old_centroids = self.centroids)
+        return self.find_closest_cluster(distance)
+
+sns.set_context('notebook')
+plt.style.use('fivethirtyeight')
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+
+# Standardize the data
+X_std = StandardScaler().fit_transform(data)
+
+# Run kmeans
+km = KMeans(n_clusters=6)
+km.fit(data)
+labels = km.predict(data)
+centroids = km.cluster_centers_
+
+# Plot the data
+fig, ax = plt.subplots(1, 2, figsize=(10, 10))
+ax[0].scatter(X_std[:, 0], X_std[:, 1])
+ax[0].scatter(X_std[:, 0], X_std[:, 1])
+ax[0].scatter(X_std[:, 0], X_std[:, 1])
+ax[0].scatter(X_std[:, 0], X_std[:, 1])
+ax[0].scatter(X_std[:, 0], X_std[:, 1])
+ax[0].scatter(X_std[:, 0], X_std[:, 1])
+ax[0].set_aspect('equal')
+ax[1].scatter(data[:, 0], data[:, 1], c=labels)
+ax[1].scatter(centroids[:, 0], centroids[:, 1], marker='o',
+                c="white", alpha=1, s=200, edgecolor='k')
+for i, c in enumerate(centroids):
+    ax[1].scatter(c[0], c[1], marker='$%d$' % i, s=50, alpha=1, edgecolor='r')
+ax[1].set_aspect('equal')
+plt.tight_layout()
 plt.show()
 
+#Effect of random initialization using K=2
+n_iter = 9
+fig, ax = plt.subplots(3, 3, figsize=(16, 16))
+ax = np.ravel(ax)
+centers = []
+for i in range(n_iter):
+    # Run local implementation of kmeans
+    km = Kmeans(n_clusters=2,
+                max_iter=3,
+                random_state=np.random.randint(0, 1000, size=1))
+    km.fit(X_std)
+    centroids = km.centroids
+    centers.append(centroids)
+    ax[i].scatter(X_std[km.labels == 0, 0], X_std[km.labels == 0, 1],
+                  c='cyan')
+    ax[i].scatter(X_std[km.labels == 1, 0], X_std[km.labels == 1, 1],
+                  c='pink')
+    ax[i].scatter(centroids[:, 0], centroids[:, 1],
+                  c='r', marker='*', s=300)
+    ax[i].set_xlim([-2, 2])
+    ax[i].set_ylim([-2, 2])
+    ax[i].legend(loc='lower right')
+    ax[i].set_title(f'{km.error:.4f}')
+    ax[i].set_aspect('equal')
+plt.tight_layout()
+plt.show()
